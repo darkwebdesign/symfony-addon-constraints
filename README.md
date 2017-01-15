c813a8412da25d8911a58add825a021dcda651a1
Raymond Schouten <raymond.schouten@medicore.nl>
2017-01-15 15:00:31 +0100

    ISSUE-11: Renamed "Symfony Add-on Validators" to "Symfony Add-on Constraints"

---
+++ README.md
@@ -0,0 +1,34 @@
+# Symfony Add-on Constraints
+
+[![Latest Stable Version](https://poser.pugx.org/darkwebdesign/symfony-addon-validators/v/stable?format=flat)](https://packagist.org/packages/darkwebdesign/symfony-addon-validators)
+[![Total Downloads](https://poser.pugx.org/darkwebdesign/symfony-addon-validators/downloads?format=flat)](https://packagist.org/packages/darkwebdesign/symfony-addon-validators)
+[![License](https://poser.pugx.org/darkwebdesign/symfony-addon-validators/license?format=flat)](https://packagist.org/packages/darkwebdesign/symfony-addon-validators)
+
+[![Build Status](https://travis-ci.org/darkwebdesign/symfony-addon-validators.svg?branch=2.3)](https://travis-ci.org/darkwebdesign/symfony-addon-validators?branch=2.3)
+[![Coverage Status](https://codecov.io/gh/darkwebdesign/symfony-addon-validators/branch/2.3/graph/badge.svg)](https://codecov.io/gh/darkwebdesign/symfony-addon-validators)
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%205.3-blue.svg)](https://php.net/)
+[![Minimum Symfony Version](https://img.shields.io/badge/symfony-%3E%3D%202.3-green.svg)](https://symfony.com/)
+
+Symfony Add-on Constraints is a collection of extra Symfony validation constraints that you can use in your Symfony
+applications.
+
+Learn more about it in its [documentation](https://github.com/darkwebdesign/symfony-addon-pack/blob/2.3/doc/reference/constraints.md).
+
+## Features
+
+* Bsn, validates that a value is a valid Dutch social security number (BSN).
+* Json, validates that a value is valid JSON.
+
+## Installing via Composer
+
+```bash
+composer require darkwebdesign/symfony-addon-constraints
+```
+
+```bash
+composer install
+```
+
+## License
+
+Symfony Add-on Constraints is licensed under the MIT License - see the `LICENSE` file for details.
---
+++ tests/BsnValidatorTest.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Copyright (c) 2017 DarkWeb Design
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace DarkWebDesign\SymfonyAddon\Constraint\Tests;
+
+use DarkWebDesign\SymfonyAddon\Constraint\Bsn;
+use DarkWebDesign\SymfonyAddon\Constraint\BsnValidator;
+use DarkWebDesign\SymfonyAddon\Constraint\Tests\Models\ToStringObject;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+class BsnValidatorTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \Symfony\Component\Validator\ExecutionContext */
+    private $context;
+
+    /** @var \DarkWebDesign\SymfonyAddon\Constraint\BsnValidator */
+    private $validator;
+
+    /** @var \DarkWebDesign\SymfonyAddon\Constraint\Bsn */
+    private $constraint;
+
+    protected function setUp()
+    {
+        $this->context = $this->getMock('Symfony\Component\Validator\ExecutionContext', array(), array(), '', false);
+        $this->validator = new BsnValidator();
+        $this->validator->initialize($this->context);
+        $this->constraint = new Bsn();
+    }
+
+    /**
+     * @param string $bsn
+     *
+     * @dataProvider providerValidate
+     */
+    public function testValidate($bsn)
+    {
+        $this->context
+            ->expects($this->never())
+            ->method('addViolation');
+
+        $this->validator->validate($bsn, $this->constraint);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerValidate()
+    {
+        return array(
+            'valid1' => array('111222333'),
+            'valid2' => array('123456782'),
+            'objectToString' => array(new ToStringObject('270590791')),
+        );
+    }
+
+    public function testValidateNull()
+    {
+        $this->context
+            ->expects($this->never())
+            ->method('addViolation');
+
+        $this->validator->validate(null, $this->constraint);
+    }
+
+    public function testValidateEmptyString()
+    {
+        $this->context
+            ->expects($this->never())
+            ->method('addViolation');
+
+        $this->validator->validate('', $this->constraint);
+    }
+
+    /**
+     * @param string $bsn
+     *
+     * @dataProvider providerValidateNoScalar
+     *
+     * @expectedException \Symfony\Component\Validator\Exception\UnexpectedTypeException
+     */
+    public function testValidateNoScalar($bsn)
+    {
+        $this->context
+            ->expects($this->never())
+            ->method('addViolation');
+
+        $this->validator->validate($bsn, $this->constraint);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerValidateNoScalar()
+    {
+        return array(
+            'array'    => array(array('foo', 'bar')),
+            'object'   => array(new stdClass()),
+            'resource' => array(tmpfile()),
+            'callable' => array(function () {}),
+        );
+    }
+
+    /**
+     * @param string $bsn
+     *
+     * @dataProvider providerValidateViolation
+     */
+    public function testValidateViolation($bsn)
+    {
+        $this->context
+            ->expects($this->once())
+            ->method('addViolation')
+            ->with(
+                $this->identicalTo($this->constraint->message),
+                $this->identicalTo(array('{{ value }}' => (string) $bsn))
+            );
+
+        $this->validator->validate($bsn, $this->constraint);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function providerValidateViolation()
+    {
+        return array(
+            'zeros'    => array('000000000'),
+            'invalid1' => array('999999999'),
+            'invalid2' => array('876543242'),
+            'toStringObject' => array(new ToStringObject('597944111')),
+        );
+    }
+}
